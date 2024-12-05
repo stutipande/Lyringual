028906de4c72aac0eba33cf603416e0f57b0efdd
Isak Erikson Winbäck <isakew@kth.se>
2024-12-05 22:39:56 +0100

    small change before google api to show what persistance should be modelled on from login comments in login view and presenter

--- src/reactjs/loginPresenter.jsx
+++ src/reactjs/loginPresenter.jsx
@@ -10,6 +10,7 @@ const Login = observer(
         
 
         return <LoginView /* mayhaps the functions? */ usernameV="" passwordV=""/>;
+        //usernameV is created here but should be a part of the model or should map to a variable in the model, that would be the identifier for each user (i have looked up how the google login api works but am too lazy to implement it right now, will do tommorow probably)
     }
 );
 
--- src/views/loginView.jsx
+++ src/views/loginView.jsx
@@ -1,10 +1,10 @@
 
 
-export function LoginView(props, props2/*temp */,usernameV, passwordV)
-{
-    function username(){/*set model.etc... */}
-    function pw(){/*set model.etc... */}
-    function goToThing(){/* go to correct place if password and such is correct etc... */window.location.hash="#/search"}
+export function LoginView(props, props2/*temp all*/,usernameV, passwordV)
+{//test user username: GustavusAdolfus pw: 1632 otherwise it does not matter since we are using google api thing, usernameV might after login be handed back as an currentUserId that is different qon each user
+    function username(a){/*set model.etc... */ usernameV=a.target.value}
+    function pw(a){/*set model.etc... */passwordV=a.target.value}
+    function goToThing(){/* go to correct place if password and such is correct etc... */if(usernameV=="GustavusAdolfus" && passwordV=="1632"){window.location.hash="#/search"}}
     
     return(
     
