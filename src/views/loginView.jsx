4044adc145413b24bc0be709e32129249d60ca3b
Isak Erikson Winbäck <isakew@kth.se>
2024-12-06 00:30:07 +0100

    this commit changes a small thing of no significance

--- src/views/loginView.jsx
+++ src/views/loginView.jsx
@@ -1,10 +1,10 @@
 
 
 export function LoginView(props, props2/*temp all*/,usernameV, passwordV)
-{//test user username: GustavusAdolfus pw: 1632 otherwise it does not matter since we are using google api thing, usernameV might after login be handed back as an currentUserId that is different qon each user
+{//test user username: aa pw: aaa otherwise it does not matter since we are using google api thing, usernameV might after login be handed back as an currentUserId that is different for each user
     function username(a){/*set model.etc... */ usernameV=a.target.value}
     function pw(a){/*set model.etc... */passwordV=a.target.value}
-    function goToThing(){/* go to correct place if password and such is correct etc... */if(usernameV=="GustavusAdolfus" && passwordV=="1632"){window.location.hash="#/search"}}
+    function goToThing(){/* go to correct place if password and such is correct etc... */if(usernameV=="aa" && passwordV=="aaa"){window.location.hash="#/search"}}
     
     return(
     
